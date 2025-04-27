ac5364aa735831761b1d38b6d0f6c94ac6e513de
Khalil <kh411@duck.com>
2025-04-27 15:24:58 -0400

    Added custom .css styling

--- build-site.el
+++ build-site.el
@@ -4,8 +4,8 @@
 ;; Customize the HTML output
 (setq org-html-validation-link nil            ;; Don't show validation link
       org-html-head-include-scripts nil       ;; Use our own scripts
-      org-html-head-include-default-style nil ;; Use our own styles
-      org-html-head "<link rel=\"stylesheet\" href=\"https://cdn.simplecss.org/simple.min.css\" />")
+      org-html-head-include-default-style nil ;; Use our own styles)
+      ;org-html-head "<link rel=\"stylesheet\" href=\"https://cdn.simplecss.org/simple.min.css\" />")
 
 ;; Define the publishing project
 (setq org-publish-project-alist
@@ -19,6 +19,7 @@
              :with-creator t            ;; Include Emacs and Org versions in footer
              :with-toc t                ;; Include a table of contents
              :section-numbers nil       ;; Don't include section numbers
+	     :css "~/format/style.css"  ;; Custom
              :time-stamp-file nil)))    ;; Don't include time stamp in file
 
 ;; Generate the site output
