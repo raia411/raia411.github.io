7c7e433ea20079fd869687ea9b5dd39d2924a5d4
Khalil <kh411@duck.com>
2025-04-27 15:32:40 -0400

    Same as last time, except website is now built

--- build-site.el
+++ build-site.el
@@ -4,7 +4,7 @@
 ;; Customize the HTML output
 (setq org-html-validation-link nil            ;; Don't show validation link
       org-html-head-include-scripts nil       ;; Use our own scripts
-      org-html-head-include-default-style nil ;; Use our own styles)
+      org-html-head-include-default-style nil) ;; Use our own styles
       ;org-html-head "<link rel=\"stylesheet\" href=\"https://cdn.simplecss.org/simple.min.css\" />")
 
 ;; Define the publishing project
